3f28883b38a6090819982089cf8c36a501cc6cc5
Al Mashruf Tonoy <tonoy5300@gmail.com>
2024-11-20 22:28:55 +0600

    server create and print message

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const os = require('os');
+const http = require('http');
+const server = http.createServer((req, res) =>{
+    res.write('Hello programmers!');
+    res.end();
+});
 
-console.log(os.cpus());
+server.listen(3000);
+
+console.log('listing on port 3000');
