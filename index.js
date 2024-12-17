1399c497fa0083badf1e95d592c24c4501089dae
Al Mashruf Tonoy <tonoy5300@gmail.com>
2024-12-17 21:46:06 +0600

    raw node proj p1

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
+
+
 const http = require('http');
-const server = http.createServer((req, res) =>{
-    res.write('Hello programmers!');
-    res.end();
-});
 
-server.listen(3000);
+const app = {};
 
-console.log('listing on port 3000');
+//config
+app.config = {
+    port: 3000
+};
+// create server
+app.createServer = () => {
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, () => {
+        console.log(`listening to port ${app.config.port}`)
+    });
+}
+
+//handing
+app.handleReqRes = (req,res) => {
+
+    //get url and parse it
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+
+    //console.log(queryStringObject);
+
+    //console.log(trimmedPath);
+    res.end('Hello world');
+};
+
+// start the server
+app.createServer();
